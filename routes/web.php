b0de3ecb38619cda7ba055a7e41945326a22315c
Viet Hung Nguyen <vhn@sfu.ca>
2025-08-17 17:51:25 -0700

    Change assets

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+
 Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
@@ -11,6 +12,10 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
+    
+    Route::get('leases', function () {
+        return Inertia::render('leases/index');
+    })->name('leases');
 });
 
 require __DIR__.'/settings.php';
