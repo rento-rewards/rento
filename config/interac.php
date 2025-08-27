1a3ca2e84dc9ff883be290676719d44d6b7b7e47
Viet Hung Nguyen <vhn@sfu.ca>
2025-08-27 00:04:06 -0700

    Update Interac configuration

--- config/interac.php
+++ config/interac.php
@@ -1,11 +1,12 @@
 <?php
 
 return [
-    'client_id'     => env('INTERAC_CLIENT_ID'),
-    'client_secret' => env('INTERAC_CLIENT_SECRET'),
-    'redirect_uri'  => env('INTERAC_REDIRECT_URI'),
-    'auth_url'      => env('INTERAC_AUTH_URL'),
-    'token_url'     => env('INTERAC_TOKEN_URL'),
-    'userinfo_url'  => env('INTERAC_USERINFO_URL'),
-    'scopes'        => env('INTERAC_SCOPES', 'openid profile'),
+    'client_id' => env('INTERAC_CLIENT_ID'),
+    'scopes' => env('INTERAC_SCOPES'),
+    'redirect' => env('INTERAC_REDIRECT_URI'),
+    'jwk_uri' => env('INTERAC_JWKS_URL'),
+    'jwt_kid' => env('INTERAC_JWT_KID'),
+    'private_key' => storage_path('app/keys/interac-private.pem'),
+    'public_key' => storage_path('app/keys/interac-public.pem'),
+    'discovery' => env('INTERAC_DISCOVERY', 'https://gateway-portal.hub-verify.innovation.interac.ca/.well-known/openid-configuration'),
 ];
