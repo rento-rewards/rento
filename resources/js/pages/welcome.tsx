bfa98d85e837f7eb61eae40738bf33ef75f26857
Viet Hung Nguyen <vhn@sfu.ca>
2025-08-31 16:45:06 -0700

    Add FeaturesSection component to landing page

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,11 +1,13 @@
 import LandingLayout from '@/layouts/landing-layout';
 import HeroSection from '@/components/pages/landing/sections/hero';
 import CallToActionSection from '@/components/pages/landing/sections/call-to-action';
+import FeaturesSection from '@/components/pages/landing/sections/features';
 
 export default function Welcome() {
     return (
         <LandingLayout>
             <HeroSection />
+            <FeaturesSection />
             <CallToActionSection />
         </LandingLayout>
     );
