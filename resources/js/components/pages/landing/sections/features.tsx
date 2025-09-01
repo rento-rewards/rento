fbafdb7fe7e4e8dc2866da55d16df9d7780e4bc3
Viet Hung Nguyen <vhn@sfu.ca>
2025-09-01 14:54:05 -0700

    Add BenefitsSection component to landing page

--- resources/js/components/pages/landing/sections/features.tsx
+++ resources/js/components/pages/landing/sections/features.tsx
@@ -2,43 +2,42 @@ import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/ca
 import { Landmark, Lock, Coins } from 'lucide-react';
 
 export default function FeaturesSection() {
-    return <section className="bg-linear-to-b to-background from-muted">
-        <div className="py-12">
-            <div className="mx-auto w-full max-w-screen-xl px-8">
-                <div>
-                    <h2 className="text-foreground mt-4 text-4xl font-semibold">Your Rent, Finally Working for You</h2>
-                    <p className="text-muted-foreground mb-12 mt-4 text-balance text-lg">Turn every rent payment into progress toward better credit and more opportunities.</p>
-                </div>
-                <div className="mt-16 grid grid-cols-1 gap-4 md:grid-cols-3">
-                    <Card>
-                        <CardHeader>
-                            <Landmark className="text-primary size-5" />
-                            <CardTitle className="mt-5">Boost Your Credit</CardTitle>
-                            <CardDescription className="mt-3">
-                                Gain points toward better loan approvals, lower interest rates, and more opportunities.
-                            </CardDescription>
-                        </CardHeader>
-                    </Card>
-                    <Card>
-                        <CardHeader>
-                            <Lock className="text-primary size-5" />
-                            <CardTitle className="mt-5">Stay Private</CardTitle>
-                            <CardDescription className="mt-3">
-                                Your landlord is never contacted. Your information stays safe and secure.
-                            </CardDescription>
-                        </CardHeader>
-                    </Card>
-                    <Card>
-                        <CardHeader>
-                            <Coins className="text-primary size-5" />
-                            <CardTitle className="mt-5">No Extra Costs</CardTitle>
-                            <CardDescription className="mt-3">
-                                No hidden fees, no credit checks—just clear, upfront pricing.
-                            </CardDescription>
-                        </CardHeader>
-                    </Card>
-                </div>
+    return <section className="bg-linear-to-b to-background from-muted py-12">
+        <div className="mx-auto w-full max-w-screen-xl px-8">
+            <div>
+                <h2 className="text-foreground mt-4 text-4xl font-semibold">Your Rent, Finally Working for You</h2>
+                <p className="text-muted-foreground mb-12 mt-4 text-balance text-lg">Turn every rent payment into
+                    progress toward better credit and more opportunities.</p>
+            </div>
+            <div className="mt-16 grid grid-cols-1 gap-4 md:grid-cols-3">
+                <Card>
+                    <CardHeader>
+                        <Landmark className="text-primary size-5" />
+                        <CardTitle className="mt-5">Boost Your Credit</CardTitle>
+                        <CardDescription className="mt-3">
+                            Gain points toward better loan approvals, lower interest rates, and more opportunities.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <Lock className="text-primary size-5" />
+                        <CardTitle className="mt-5">Stay Private</CardTitle>
+                        <CardDescription className="mt-3">
+                            Your landlord is never contacted. Your information stays safe and secure.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
+                <Card>
+                    <CardHeader>
+                        <Coins className="text-primary size-5" />
+                        <CardTitle className="mt-5">No Extra Costs</CardTitle>
+                        <CardDescription className="mt-3">
+                            No hidden fees, no credit checks—just clear, upfront pricing.
+                        </CardDescription>
+                    </CardHeader>
+                </Card>
             </div>
         </div>
-    </section>
+    </section>;
 };
--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -3,6 +3,7 @@ import HeroSection from '@/components/pages/landing/sections/hero';
 import CallToActionSection from '@/components/pages/landing/sections/call-to-action';
 import FeaturesSection from '@/components/pages/landing/sections/features';
 import HowItWorks from '@/components/pages/landing/sections/how-it-works';
+import { BenefitsSection } from '@/components/pages/landing/sections/benifits';
 
 export default function Welcome() {
     return (
@@ -10,6 +11,7 @@ export default function Welcome() {
             <HeroSection />
             <FeaturesSection />
             <HowItWorks />
+            <BenefitsSection />
             <CallToActionSection />
         </LandingLayout>
     );
