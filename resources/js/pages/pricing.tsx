5963ef4e3ef778449fcf86e5db522ce9c468271e
Viet Hung Nguyen <vhn@sfu.ca>
2025-09-02 16:20:17 -0700

    Refactor pricing component to use RadioGroup for plan selection

--- resources/js/pages/pricing.tsx
+++ resources/js/pages/pricing.tsx
@@ -1,11 +1,10 @@
 import LandingLayout from '@/layouts/landing-layout';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
 import { Link } from '@inertiajs/react';
 import { register } from '@/routes';
-import { Badge } from '@/components/ui/badge';
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group';
 
 export default function Pricing() {
     const [choice, setChoice] = useState('monthly');
@@ -13,29 +12,45 @@ export default function Pricing() {
     return <LandingLayout>
         <div className="flex flex-col gap-6 items-center py-12 w-full mx-auto px-8 @container
             bg-radial-[125%_125%_at_50%_10%] from-background to-muted">
-            <h2 className="text-foreground text-2xl @sm:text-4xl font-semibold text-center text-balance">Simple Plan, Transparent Pricing</h2>
-            <p className="text-muted-foreground text-balance text-md @sm:text-lg text-center">Choose the plan that works best for you.</p>
-            <Tabs value={choice} onValueChange={setChoice} className="w-full max-w-[375px] text-sm text-muted-foreground">
-                <TabsList className="grid w-full grid-cols-2">
-                    <TabsTrigger value="monthly">Monthly</TabsTrigger>
-                    <TabsTrigger value="annually">Annually</TabsTrigger>
-                </TabsList>
-            </Tabs>
-            <div className="text-sm">
-                <span className="font-medium text-primary">Save 40%</span> on Annual Billing
+            <h2 className="text-foreground text-2xl @sm:text-4xl font-semibold text-center text-balance">Simple Plan,
+                Transparent Pricing</h2>
+            <p className="text-muted-foreground text-balance text-md @sm:text-lg text-center">Choose the plan that works
+                best for you.</p>
+            <div className="bg-input/50 inline-flex h-9 rounded-md p-0.5">
+                <RadioGroup
+                    value={choice}
+                    onValueChange={setChoice}
+                    className="group after:bg-background has-focus-visible:after:border-ring has-focus-visible:after:ring-ring/50 relative inline-grid grid-cols-[1fr_1fr] items-center gap-0 text-sm font-medium after:absolute after:inset-y-0 after:w-1/2 after:rounded-sm after:shadow-xs after:transition-[translate,box-shadow] after:duration-300 after:ease-[cubic-bezier(0.16,1,0.3,1)] has-focus-visible:after:ring-[3px] data-[state=off]:after:translate-x-0 data-[state=on]:after:translate-x-full"
+                    data-state={choice === "monthly" ? 'off' : 'on'}>
+                    <label
+                        className="group-data-[state=on]:text-muted-foreground/70 relative z-10 inline-flex h-full min-w-8 cursor-pointer items-center justify-center px-4 whitespace-nowrap transition-colors select-none">
+                        Monthly
+                        <RadioGroupItem id="monthly" value="monthly" className="sr-only" />
+                    </label>
+                    <label
+                        className="group-data-[state=off]:text-muted-foreground/70 relative z-10 inline-flex h-full min-w-8 cursor-pointer items-center justify-center px-4 whitespace-nowrap transition-colors select-none">
+                            Yearly{' '}
+                              <span
+                                  className="ml-2 group-data-[state=off]:text-muted-foreground/70 transition-colors group-data-[state=on]:text-emerald-500">
+                              -40%
+                            </span>
+                        <RadioGroupItem id="annually" value="annually" className="sr-only" />
+                    </label>
+                </RadioGroup>
             </div>
             <Card className="p-8 @md:p-10">
                 <CardHeader>
                     <CardTitle className="text-xl text-center">All-in-One Solution</CardTitle>
-                    <CardDescription className="text-muted-foreground text-center">One simple plan for all your needs.</CardDescription>
+                    <CardDescription className="text-muted-foreground text-center">One simple plan for all your
+                        needs.</CardDescription>
                 </CardHeader>
                 <CardContent className="flex flex-col items-center">
                     <div className="flex gap-4 items-center justify-center mx-auto">
-                        <div className="text-5xl font-bold text-right">${choice === "monthly" ? 5 : 3}</div>
+                        <div className="text-5xl font-bold text-right">${choice === 'monthly' ? 5 : 3}</div>
                         <div className="text-left">
                             <div className="text-sm">Per month</div>
                             <div className="text-xs text-muted-foreground">
-                                {choice === "monthly" ? "Billed monthly" : "Billed annually"}
+                                {choice === 'monthly' ? 'Billed monthly' : 'Billed annually'}
                             </div>
                         </div>
                     </div>
@@ -46,9 +61,10 @@ export default function Pricing() {
                     </Button>
                 </CardContent>
                 <CardFooter className="flex-col gap-2">
-                    <p className="text-xs @sm:text-sm text-center text-muted-foreground text-balance">All plans include a 14-day free trial. No credit card required. No hidden fees. Cancel anytime.</p>
+                    <p className="text-xs @sm:text-sm text-center text-muted-foreground text-balance">All plans include
+                        a 14-day free trial. No credit card required. No hidden fees. Cancel anytime.</p>
                 </CardFooter>
             </Card>
         </div>
-    </LandingLayout>
+    </LandingLayout>;
 }
