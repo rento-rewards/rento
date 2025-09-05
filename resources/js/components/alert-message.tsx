8165780a79bee05d5d487ee51865738a1390cef7
Viet Hung Nguyen <vhn@sfu.ca>
2025-09-05 04:34:46 -0700

    Add alert message component

---
+++ resources/js/components/alert-message.tsx
@@ -0,0 +1,46 @@
+import { cva, VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+import { ComponentProps } from 'react';
+import { cn } from '@/lib/utils';
+import { CircleAlertIcon, CircleCheckIcon, InfoIcon, TriangleAlertIcon } from 'lucide-react';
+
+const alertMessageVariant = cva(
+    'rounded-md border px-4 py-3 text-sm',
+    {
+        variants: {
+            variant: {
+                info: 'border-blue-500/50 bg-blue-50/50 text-blue-600',
+                success: 'border-emerald-500/50 bg-emerald-50/50 text-emerald-600',
+                warning: 'border-yellow-500/50 bg-yellow-50/50 text-yellow-600',
+                error: 'border-red-500/50 bg-red-50/50 text-red-600'
+            }
+        },
+        defaultVariants: {
+            variant: 'info'
+        }
+    }
+);
+
+type Props = ComponentProps<'div'> & VariantProps<typeof alertMessageVariant>
+
+type Variant = VariantProps<typeof alertMessageVariant>['variant']
+
+export default function AlertMessage({ className, children, variant }: Props) {
+    const Icon = variant === 'success' ? CircleCheckIcon
+        : variant === 'warning' ? TriangleAlertIcon
+            : variant === 'error' ? CircleAlertIcon
+                : InfoIcon;
+
+    return (
+        <div className={cn(alertMessageVariant({ className, variant }))}>
+            <div>
+                <Icon
+                    className="me-3 -mt-0.5 inline-flex opacity-60"
+                    size={16}
+                    aria-hidden="true"
+                />
+                {children}
+            </div>
+        </div>
+    );
+}
--- resources/js/pages/settings/subscription.tsx
+++ resources/js/pages/settings/subscription.tsx
@@ -3,10 +3,10 @@ import AppLayout from '@/layouts/app-layout';
 import SettingsLayout from '@/layouts/settings/layout';
 import HeadingSmall from '@/components/heading-small';
 import { Head } from '@inertiajs/react';
-import { CircleAlert, CircleCheckIcon } from 'lucide-react';
 import NewSubscription from '@/components/subscriptions/new-subscription';
 import SubscriptionData = App.Data.Subscription.SubscriptionData;
 import CurrentPlan from '@/components/subscriptions/current-plan';
+import AlertMessage from '@/components/alert-message';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -34,27 +34,12 @@ export default function Subscription(props: Props) {
             <SettingsLayout>
                 <div className="space-y-6 @container">
                     <HeadingSmall title="Subscription" description="Update your account's subscription settings" />
-                    {flash.success &&
-                        <div className="rounded-md border border-emerald-500/50 px-4 py-3 text-emerald-600">
-                            <p className="text-sm">
-                                <CircleCheckIcon
-                                    className="me-3 -mt-0.5 inline-flex opacity-60"
-                                    size={16}
-                                    aria-hidden="true"
-                                />
-                                {flash.success}
-                            </p>
-                        </div>}
-                    {flash.error && <div className="rounded-md border border-red-500/50 px-4 py-3 text-red-600">
-                        <p className="text-sm">
-                            <CircleAlert
-                                className="me-3 -mt-0.5 inline-flex opacity-60"
-                                size={16}
-                                aria-hidden="true"
-                            />
-                            {flash.error}
-                        </p>
-                    </div>}
+                    {flash.success && <AlertMessage variant="success">
+                        {flash.success}
+                    </AlertMessage>}
+                    {flash.error && <AlertMessage variant="error">
+                        {flash.error}
+                    </AlertMessage>}
                     {current_plan ? <CurrentPlan {...current_plan} /> : <NewSubscription />}
                 </div>
             </SettingsLayout>
